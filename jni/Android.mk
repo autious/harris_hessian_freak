34daca43ec7280ae990748e7a020fcc512f1fe9e
Max Danielsson <max@autious.net>
2015-04-09 15:35:27 +0200

    moves source list to separate mk file

--- jni/Android.mk
+++ jni/Android.mk
@@ -7,7 +7,11 @@ LOCAL_C_INCLUDES := jni/include/
 #THe android_io include is intended to mask some stdio functions 
 LOCAL_CFLAGS += -std=c99 -Wall -DPROFILE -include "src/android_io.h"
 
-LOCAL_SRC_FILES := src/android_opencl_test_jni.c src/opencl_error.c src/opencl_loader.c src/opencl_test.c src/lodepng.c src/gauss_kernel.c src/opencl_util.c src/opencl_program.c src/opencl_fd.c src/harris_hessian_freak.c src/util.c src/freak.c src/opencl_timer.c src/android_io.c src/stb_image.c src/compile_flag_object.c
+include Sources.mk
+
+LOCAL_SRC_FILES := $(addprefix src/,$LIBRARY_SOURCES)
+LOCAL_SRC_FILES += src/android_opencl_test_jni.c src/android_io.c
+
 LOCAL_MODULE := android_opencl_test_jni
 LOCAL_LDLIBS := -landroid -ldl -llog -lOpenCL
 
